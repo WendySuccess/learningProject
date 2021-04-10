dbb92123899b7a98773414fbb70ae76cf77574f9
soon ming poo <soonmingpoo@yahoo.com>
2021-04-10 20:47:15 +0800

    new folder

--- ReturnType/src/overloadMethod.java
+++ ReturnType/src/overloadMethod.java
@@ -1,6 +1,16 @@
 //method overloading
 public class overloadMethod {
 	
+	int val = 10;
+	
+	overloadMethod(){
+		val = 1;
+	}
+	
+	overloadMethod(int val){
+		this.val = val;
+	}
+	
 public void area(int b,int h)
     {
          System.out.println("Area of Triangle : "+(0.5*b*h));
@@ -16,5 +26,9 @@ public void area(int b,int h)
 overloadMethod ob=new overloadMethod();
        ob.area(10,12);
        ob.area(5);  
+       
+       overloadMethod ob2 =new overloadMethod(10);
+       ob2.area(10,12);
+       ob2.area(5);  
    }
 }
