9f8add97621b7c85e59dab9c7df36d320299c54f
soon ming poo <soonmingpoo@yahoo.com>
2021-04-25 21:58:12 +0800

    20210425 - 1

---
+++ JavaLiveClass8/src/LinearSearch.java
@@ -0,0 +1,31 @@
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int[] Arr  = {10 , 20 , 5 , 70 , 60 ,80 , 100 , 90 , 200 , 700};
+		 
+		 for (int j = 0 ; j <Arr.length ; j ++) {
+		    	if (Arr[j] != 0)
+		    System.out.print(Arr[j] + " ");
+		    }
+		 System.out.println();
+		 search(Arr  ,90);
+		 search(Arr  ,9022  );
+	}
+	
+	static void search(int[] Arr ,int searchint) {
+		boolean check = false ;
+		   for(int i = 0 ; i <Arr.length ; i ++) {  	
+		if(Arr[i] == searchint) {
+			 System.out.println("The number " +  searchint + " at number " + (i +1) + " of the array" );
+			 check = true;
+		}
+		
+	}
+		   if (check == false)
+				System.out.println("The number " +  searchint + " at number is not found in the array" );
+
+}
+	
+}
