88138a3f145606e539d709446cf75a3add55a27e
soon ming poo <soonmingpoo@yahoo.com>
2021-05-02 23:03:46 +0800

    20210502 -1

--- RepositoriesPrototype/src/Repositories.java
+++ RepositoriesPrototype/src/Repositories.java
@@ -1,24 +1,128 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Repositories {
-
-	Repositories() {
+	static Scanner sc= new Scanner(System.in); 
+	public void viewAllfile(File dir) {
+		System.out.println();
+		try {
+			File[] files = dir.listFiles();
+			int n = files.length;
+			String[] filesNames = new String [n];
+			int i = 0 ; 
+			for (File file : files) {
+	               filesNames[i] = file.getName();
+	               i ++;
+	            } 	
+			//Retrieving the file names in an ascending order (QuickSort)
+			sort(filesNames,0,n-1);
+			printFileList(filesNames);
+			System.out.println();
+	    }catch (NullPointerException ex) {
+	    	System.out.println("File Repository not been setup. Kindly check with admin.");
+	    }
 	}
 	
-void viewfile() {
+	private void printFileList(String[] arr) {
+		int n = arr.length;
+		System.out.println("List of File");
+		for (int i = 0; i <n ;++i ) {
+			System.out.println((i+1) + ". "+arr[i]);
+		}
+		System.out.println();
+	}
+	
+	private void sort(String[] arr, int low, int high) {
+		if(low< high){	
+			int pi = partition(arr,low, high);
+			sort(arr,low,pi -1);
+			sort(arr,pi , high);
+		}
+	}
+	
+	private int partition(String[] arr, int low, int high) {
+		String pivot = arr[high];
+		int i = (low-1);
+		for(int j = low ; j < high ; j++)
+		{
+			if(arr[j].compareTo(pivot)<0) {
+				i ++;
+				String temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
 		
+		String temp = arr[i+1];
+		arr[i+1] = arr[high];
+		arr[high] = temp;
+		return i+1;
 	}
 	//Option
 	//Option to add a user specified file to the application
-	void addfile() {
+	public void addfile(File dir) {
+		System.out.print("Please enter new File name : ");
+		String filename = sc.nextLine(); 
+		File myFile = new File(dir.getAbsolutePath() + "/" +filename);
+		try {
+			if(myFile.createNewFile()) { //create file
+				System.out.println("New File is created! File Name : " + filename);
+				System.out.println();
+			}
+			else
+			{
+				System.out.println("File existing!");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Unable to create file. Kindly check with admin.");
+		} 
 		
+		appendFile(dir.getAbsolutePath() + "/" +filename);
 	}
 	//Option to delete a user specified file from the application
 	
-	void deletefile() {
+	private void appendFile(String filename) {
+		
+		String originaldata = ""; 
+		String newdata = ""; 
+		try (BufferedReader in2 = new BufferedReader(new FileReader(filename))){
+			
+			String s2= in2.readLine();		
+						while (s2 !=null) {
+					originaldata = originaldata + s2 + System.lineSeparator();
+					//System.out.println("Read :" +s2);
+					s2 = in2.readLine();	
+					
+				}}catch (Exception eX) {
+					
+					eX.printStackTrace();
+				} 		
+			
+		try(FileOutputStream fout =  new FileOutputStream(filename);){
+			
+			System.out.println("please key in some new data"); 
+			String res = sc.nextLine(); 
+				  newdata  = originaldata + res + System.lineSeparator();
+			  fout.write( newdata.getBytes());
+			  System.out.println("Success Added."); 
+	
+			} catch (IOException eX2) {
+					eX2.printStackTrace();
+		}
+		
 		
 	}
 	
-	void seachfile() {
+	public void deletefile() {
+		
+	}
+	
+	public void seachfile() {
 		
 	}
 }
--- RepositoriesPrototype/src/RepositoriesPrototype.java
+++ RepositoriesPrototype/src/RepositoriesPrototype.java
@@ -1,10 +1,14 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class RepositoriesPrototype {
+	private static final String String = null;
 	static Scanner sc= new Scanner(System.in); 
+	static File currentDir = new File("./FileStored");
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		welcome();
+		Repositories obj = new Repositories();
+		Menu(obj);
 		
 	}
 
@@ -15,7 +19,96 @@ public class RepositoriesPrototype {
 		System.out.println("                              Developer: Soon Ming Poo                              ");
 		System.out.println("*************************************************************************************");
 		System.out.println();
+	}
+	
+	public static void Menu(Repositories obj ) {	
+		obj.viewAllfile(currentDir);
+		System.out.println("*********************************** Main Menu ***************************************");
+		System.out.println("1. Add New File ");  
+		System.out.println("2. Delete Existing File");  
+		System.out.println("3. Search From Existing Files");  
+		System.out.println("4. Exit Program");  
+		System.out.println("*************************************************************************************");
 		System.out.println();
+		System.out.print("Enter Your Choice : ");  
+		char res = sc.next().charAt(0); 
+
+		switch (res )
+		{
+		case '1' :
+			obj.addfile(currentDir);
+		    break;
+		case '2' :
+			obj.deletefile();
+		    break;
+		case '3' :
+			obj.seachfile() ;
+		    break;
+		case '4' :
+			ExitProgram();
+		    break;
+		default: 
+			//System.out.println("Invalid selection.");  
+			
+		}
+		Retry(obj, res);
+	}
+	
+	public static void Retry(Repositories obj , char res) {		
+		switch (res )
+		{
+		case '1' :
+			System.out.println("1. Add More File ");  
+		    break;
+		case '2' :
+			System.out.println("1. Delete More Existing File");  
+		    break;
+		case '3' :
+			System.out.println("1. Search More Files"); 
+		    break;
+		default: 
+			System.out.println("Invalid selection.");  	
+		}
+		
+		System.out.println("2. Back to Main Menu");  
+		System.out.println("3. Exit Program");  
+		//System.out.print("Do you want try again ? Y/N "); 
+		char res1 = sc.next().charAt(0); 
+
+		switch (res1 )
+		{
+		case '1' :
+			switch (res )
+			{
+			case '1' :
+				obj.addfile(currentDir);  
+			    break;
+			case '2' :
+				obj.deletefile();
+			    break;
+			case '3' :
+				obj.seachfile() ;
+			    break;
+			default: 
+				System.out.println("Invalid selection.");  	
+			}
+		    break;
+		case '2' :
+			Menu(obj);
+		    break;
+		case '3' :
+			ExitProgram();
+		    break;
+		default: 
+			//System.out.println("Invalid selection.");  
+			
+		}
+		Retry(obj, res);
+	}
+	
+	public static void ExitProgram() {
+		System.out.print("Thank you for using. See You Soon");  
+		System.exit(1);
 	}
 
 }
